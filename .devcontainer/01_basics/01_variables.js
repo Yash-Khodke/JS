f0ce678017b5027f3779c17d5001705c094e91e8
Yash Laxman Khodke <yashkhodke44@gmail.com>
2024-06-18 07:39:22 +0000

    01_basics

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "yashkhodke44@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// console.log - output at Screen
+// accountId = 2
+
+console.log(accountId);
